3e9ed8b45926d3e577b420c5b8f038cb9b33c103
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-03-27 15:20:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 //your code here!
+const list=document.getElementById("infi-list");
+let itemCount=0;
+
+function addlistItems(numItems) {
+	for(let i=0;i<numItems;i++)
+		{
+			const listItem=document.createElement('li');
+			listItem.textContent=`Item ${++itemCount}`;
+			list.appendChild(listItem);
+		}
+}
+
+addlistItems(10)
+
+window.addEventListener('scroll',()=>{
+	if(window.innerHTML + window.scrolly >= document.body.offsetHeight)
+	{
+		addlistItems(2);
+	}
+});
+
+
+
+
+
 
